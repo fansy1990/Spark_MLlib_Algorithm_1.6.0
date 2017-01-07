c9d9eab158498abdf2641718850b056d42cc6acb
yizhongxunsi <yizhongxunsi@gmail.com>
2017-01-07 14:36:47 +0800

    提交NaiveBayes算法封闭

---
+++ src/main/scala/com/fz/classification/CustomNaiveBayes.scala
@@ -0,0 +1,56 @@
+package com.fz.classification
+
+import com.fz.util.Utils
+import org.apache.spark.mllib.classification.NaiveBayes
+import org.apache.spark.mllib.optimization.L1Updater
+
+/**
+ * 朴素贝叶斯封装算法
+ * Labels used in Logistic Regression should be {0, 1, ..., k - 1} for k classes multi-label classification problem
+ * 输入参数：
+ * testOrNot : 是否是测试，正常情况设置为false
+ * input：输出数据；
+ * minPartitions : 输入数据最小partition个数
+ * output：输出路径
+ * targetIndex：目标列所在下标，从1开始
+ * splitter：数据分隔符；
+ * modelType：朴素贝叶斯类型（多项式和伯努利）："multinomial" or "bernoulli"
+ * lambda : 平滑度，doule类型，通常为1.0
+ *
+ * Created by cuihuan on 2017/1/7.
+ */
+object CustomNaiveBayes {
+  def main(args: Array[String]) {
+    if (args.length != 8) {
+      println("Usage: com.fz.classification.NaiveBayes testOrNot input minPartitions output targetIndex " +
+        "splitter modelType lambda")
+      System.exit(-1)
+    }
+
+    val testOrNot = args(0).toBoolean // 是否是测试，sparkContext获取方式不一样, true 为test
+    val input = args(1)
+    val minPartitions = args(2).toInt
+    val output = args(3)
+    val targetIndex = args(4).toInt // 从1开始，不是从0开始要注意
+    val splitter = args(5)
+    val modelType = args(6) //
+    val lambda = args(7).toDouble
+    // 删除输出，不在Scala算法里面删除，而在Java代码里面删除
+    //     Utils.deleteOutput(output)
+
+    val sc =  Utils.getSparkContext(testOrNot,"NaiveBayes create Model")
+
+    // construct data
+    // Load and parse the data
+    val training = Utils.getLabeledPointData(sc,input,minPartitions,splitter,targetIndex).cache()
+
+//     Run training algorithm to build the model
+    val modelTypes =Array("multinomial","bernoulli").toList//朴素贝叶斯的类型{"multinomial","bernoulli"}
+    val model = modelTypes.contains(modelType) match {
+      case true => NaiveBayes.train(training,lambda,modelType)
+      case false =>throw new RuntimeException("the model '"+modelType+"' of NaiveBayes is undefined.Only the \"multinomial\"or \"bernoulli\" is  available ")
+    }
+    model.save(sc,output)
+    sc.stop()
+  }
+}
---
+++ src/test/scala/com/fz/classification/CustomNaiveBayesTest.scala
@@ -0,0 +1,54 @@
+package com.fz.classification
+
+import java.io.File
+
+import com.fz.util.Utils
+import org.junit.Assert._
+import org.junit.Test
+
+/**
+ * 测试Bayes算法
+ * Created by cuihuan on 2017/1/7.
+ */
+@Test
+class CustomNaiveBayesTest {
+  @Test
+  def testMain1()={
+    //    testOrNot input output targetIndex splitter method hasIntercept numClasses
+    val args = Array(
+      "true",
+      "./src/data/bayes.dat",
+      "2",
+      "./target/bayes/tmp1",
+      "1",
+      " ",
+      "multinomial",
+      "1.0"
+    )
+    // 删除输出目录
+    Utils.deleteOutput(args(3))
+    CustomNaiveBayes.main(args)
+    val exist = new File(args(3)).exists()
+    assertTrue(exist)
+  }
+
+  @Test
+  def testMain2()={
+    //    testOrNot input output targetIndex splitter method hasIntercept numClasses
+    val args = Array(
+      "true",
+      "./src/data/naiveBayesBer.dat",
+      "2",
+      "./target/bayes/tmp1",
+      "1",
+      " ",
+      "bernoulli",
+      "2.0"
+    )
+    // 删除输出目录
+    Utils.deleteOutput(args(3))
+    CustomNaiveBayes.main(args)
+    val exist = new File(args(3)).exists()
+    assertTrue(exist)
+  }
+}
