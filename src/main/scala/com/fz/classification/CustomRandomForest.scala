9415288e5cf1076fe0e087ae9dc5bf3f0f3d7d0a
yizhongxunsi <yizhongxunsi@gmail.com>
2017-01-17 11:40:26 +0800

    增加决策树的算法封装

---
+++ src/main/scala/com/fz/classification/CustomRandomForest.scala
@@ -0,0 +1,26 @@
+package com.fz.classification
+
+/**
+ * 随机森林封装算法   可用于
+ *      二元分类：
+ *      多元分类：
+ *      回归：
+ * 输入参数：
+ * testOrNot : 是否是测试，正常情况设置为false
+ * input：输出数据；
+ * minPartitions: 输入数据最小partition个数
+ * output：输出路径
+ * targetIndex：目标列所在下标，从1开始
+ * splitter：数据分隔符；
+ * impurity：不纯度度函数，分类的有：信息熵(entropy)和基尼指数(gini)
+ * maxDepth：树的最大深度，
+ * numClasses: 类别的个数
+ * maxBins：设定分裂数据集  suggestion 32
+ *
+ * Created by cuihuan on 2017/1/17.
+ */
+object CustomRandomForest {
+  def main(args: Array[String]) {
+
+  }
+}
